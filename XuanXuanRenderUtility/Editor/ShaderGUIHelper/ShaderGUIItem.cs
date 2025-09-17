fd0fb6f56dc99c5745b984a927ec367d05223f16
xuanshushu <910071006@qq.com>
2025-09-18 01:04:40 +0800

    Add ShaderGUIHelper FrameWork

---
+++ XuanXuanRenderUtility/Editor/ShaderGUIHelper/ShaderGUIItem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+namespace NBShaderEditor
+{
+    public abstract class ShaderGUIItem
+    {
+        public MaterialEditor MatEditor;
+        public MaterialProperty Property;
+        public ShaderGUIItem ParentItem;
+        public List<ShaderGUIItem> ChildrenItemList;
+        public Material[] Mats;
+
+        public ShaderGUIItem(MaterialEditor editor,MaterialProperty[] props,ShaderGUIItem parentItem,string propertyname = null)
+        {
+            if (propertyname != null)
+            {
+                foreach (var prop in props)
+                {
+                    if (prop.name == propertyname)
+                    {
+                        Property = prop;
+                        return;
+                    }
+                }
+            }
+
+            MatEditor = editor;
+            ParentItem = parentItem;
+            Mats = MatEditor.targets as Material[];
+            
+        }
+
+        public virtual void OnGUI()
+        {
+            
+        }
+    }
+}
