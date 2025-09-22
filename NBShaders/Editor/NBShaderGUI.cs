a52e55054721b350d63d85b3aa74ff2ee88ae399
XuanXuanInCompany <910071006@qq.com>
2025-09-22 20:38:20 +0800

    2.0.0 WIP Dev

--- NBShaders/Editor/NBShaderGUI.cs
+++ NBShaders/Editor/NBShaderGUI.cs
@@ -1,4 +1,6 @@
 using UnityEditor;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace NBShaderEditor
 {
@@ -11,5 +13,29 @@ namespace NBShaderEditor
             NBRootItem.OnGUI(materialEditor, properties);
             
         }
+    }
+    
+    public class NBShaderRootItem:ShaderGUIRootItem
+    {
+        public ShaderGUIBigBlockItem modeBigBlock;
+        public override void InitFlags(List<Material> mats)
+        {
+            ShaderFlags = new List<ShaderFlagsBase>();
+            foreach (Material mat in mats)
+            {
+                W9ParticleShaderFlags flag = new W9ParticleShaderFlags(mat);
+                ShaderFlags.Add(flag);
+            }
+        }
+
+        public override void OnChildOnGUI()
+        {
+            if (IsInit)
+            {
+                modeBigBlock = new ModeBigBlockItem(this,null);
+            }
+            modeBigBlock.OnGUI();
+        }
+        
     }
 }
