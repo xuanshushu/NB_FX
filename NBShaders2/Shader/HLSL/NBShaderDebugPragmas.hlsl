d2dafa01adbbc7d5517919332d6f4a02c7e70c6a
xuanshushu <910071006@qq.com>
2026-05-31 17:20:42 +0800

    去掉Symbol

--- NBShaders2/Shader/HLSL/NBShaderDebugPragmas.hlsl
+++ NBShaders2/Shader/HLSL/NBShaderDebugPragmas.hlsl
@@ -2,5 +2,4 @@
 // This file is intentionally included with #include_with_pragmas.
 
 #undef NB_SHADER_DEBUG_SYMBOLS
-#define NB_SHADER_DEBUG_SYMBOLS 1
-#pragma enable_d3d11_debug_symbols
+#define NB_SHADER_DEBUG_SYMBOLS 0
